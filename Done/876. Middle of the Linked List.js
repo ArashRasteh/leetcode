d9d366692511266a133663bd831bd87b123607aa
Arash Rasteh <ashrasteh@gmail.com>
2024-10-18 17:43:25 -0600

    Measuring 876 performance

--- Done/876. Middle of the Linked List.js
+++ Done/876. Middle of the Linked List.js
@@ -31,3 +31,27 @@ var middleNode = function(head) {
 
   return curr;
 };
+
+function ListNode(val, next) {
+  this.val = (val===undefined ? 0 : val)
+  this.next = (next===undefined ? null : next)
+}
+
+let head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+let t0 = process.hrtime.bigint();
+middleNode(head)
+let t1 = process.hrtime.bigint();
+console.log(`Time: ${(t1 - t0)} nanoseconds`);
+console.log(middleNode(head)); // 3
+
+let head2 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6))))));
+t0 = process.hrtime.bigint();
+middleNode(head2)
+t1 = process.hrtime.bigint();
+console.log(`Time: ${t1 - t0} nanoseconds`);
+console.log(middleNode(head2)); // 4
+
+
+t0 = process.hrtime.bigint();
+t1 = process.hrtime.bigint();
+console.log(`Time: ${t1 - t0} nanoseconds`);
